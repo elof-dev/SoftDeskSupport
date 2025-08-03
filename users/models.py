c11fd9f8261d8dee6cdc72f99cce15884d8c5d1b
elof-dev <elodie.fourcade65@gmail.com>
2025-08-03 17:22:38 +0200

    ok

--- users/models.py
+++ users/models.py
@@ -2,6 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    age = models.PositiveIntegerField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
     can_be_contacted = models.BooleanField(default=False)
     can_data_be_shared = models.BooleanField(default=False)
--- users/serializers.py
+++ users/serializers.py
@@ -5,10 +5,23 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
-            'id', 'username', 'password', 'email', 'age',
+            'id', 'username', 'password', 'email', 'birth_date',
             'can_be_contacted', 'can_data_be_shared', 'first_name', 'last_name'
         ]
-        extra_kwargs = {'password': {'write_only': True}}
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'birth_date': {'required': True}
+        }
+
+    def validate_birth_date(self, value):
+        from datetime import date, timedelta
+        today = date.today()
+        age = today.year - value.year - ((today.month, today.day) < (value.month, value.day))
+        if age < 15:
+            raise serializers.ValidationError("Vous devez avoir au moins 15 ans pour vous inscrire.")
+        return value
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
